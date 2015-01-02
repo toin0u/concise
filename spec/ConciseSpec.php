cba55172c8a889bb0ba88926315233e94ba657ad
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-01-03 00:59:24 +0100

    Updates Concise spec

--- spec/ConciseSpec.php
+++ spec/ConciseSpec.php
@@ -4,7 +4,6 @@ namespace spec\Concise;
 
 use Concise\Provider;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class ConciseSpec extends ObjectBehavior
 {
@@ -16,5 +15,19 @@ class ConciseSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Concise\Concise');
+    }
+
+    function it_should_allow_to_shorten_a_url(Provider $provider)
+    {
+        $provider->shorten('http://any.url')->willReturn("http://goo.gl/shortened");
+
+        $this->shorten('http://any.url')->shouldReturn("http://goo.gl/shortened");
+    }
+
+    function it_should_allow_to_expand_a_url(Provider $provider)
+    {
+        $provider->expand('http://goo.gl/shortened')->willReturn("http://any.url");
+
+        $this->expand('http://goo.gl/shortened')->shouldReturn("http://any.url");
     }
 }
